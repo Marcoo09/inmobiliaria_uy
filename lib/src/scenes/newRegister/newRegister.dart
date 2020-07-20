de18246ae43938b980438582548f4137ad46e97a
Marco Fiorito <marcofiorito1@gmail.com>
2020-07-19 23:16:03 -0300

    stepper structure and combo box, form basic widgets

--- lib/src/scenes/newRegister/newRegister.dart
+++ lib/src/scenes/newRegister/newRegister.dart
@@ -1,23 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:inmobiliariauy/src/reusableWidgets/comboBox.dart';
 
-class NewRegister extends StatelessWidget {
+class NewRegister extends StatefulWidget {
+  @override
+  _NewRegisterState createState() => new _NewRegisterState();
+}
+
+class _NewRegisterState extends State<NewRegister> {
+  List<Step> steps = [
+    Step(
+      title: const Text('Step 1'),
+      isActive: true,
+      state: StepState.complete,
+      content: Column(
+        children: <Widget>[
+          ComboBox(title:'Modalidad',values:['Alquiler', 'Venta']),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      isActive: false,
+      state: StepState.editing,
+      title: const Text('Step 2'),
+      content: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Home Address'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Postcode'),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      isActive: false,
+      state: StepState.editing,
+      title: const Text('Step 3'),
+      content: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Home Address'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Postcode'),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      isActive: false,
+      state: StepState.editing,
+      title: const Text('Step 4'),
+      content: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Home Address'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Postcode'),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      isActive: false,
+      state: StepState.editing,
+      title: const Text('Step 5'),
+      content: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Home Address'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Postcode'),
+          ),
+        ],
+      ),
+    ),
+  ];
+  int currentStep = 0;
+  bool complete = false;
+
+  next() {
+    currentStep + 1 != steps.length
+        ? goTo(currentStep + 1)
+        : setState(() => complete = true);
+  }
+
+  cancel() {
+    if (currentStep > 0) {
+      goTo(currentStep - 1);
+    }
+  }
+
+  goTo(int step) {
+    setState(() => currentStep = step);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Registro Nuevo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Acá va a haber un formulario',
-            ),
-          ],
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text('Nuevo Registro'),
         ),
-      ),
-    );
+        body: Column(children: <Widget>[
+          Expanded(
+            child: Stepper(
+              steps: steps,
+              currentStep: currentStep,
+              onStepContinue: next,
+              onStepTapped: (step) => goTo(step),
+              onStepCancel: cancel,
+            ),
+          ),
+        ]));
   }
 }
