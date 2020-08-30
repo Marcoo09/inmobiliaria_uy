32c0486f914cd06217e9eb2b38d9d432452e622f
Marco Fiorito <marcofiorito1@gmail.com>
2020-08-30 18:38:24 -0300

    refactor odf folders

--- lib/src/ui/scenes/home/home.dart
+++ lib/src/ui/scenes/home/home.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:inmobiliariauy/src/scenes/newRegister/newRegister.dart';
+import 'package:inmobiliariauy/src/ui/scenes/new_register/new_register.dart';
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +21,8 @@ class Home extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-           context,
-           MaterialPageRoute(builder: (context) => NewRegister()),
+            context,
+            MaterialPageRoute(builder: (context) => NewRegister()),
           );
         },
         tooltip: 'Nuevo registro',
